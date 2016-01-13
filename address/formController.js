e9a98f140b88944085076962550ed07a47fbd8c4
Astha Sharma <a@astha.me>
2016-01-13 19:17:54 +0530

    Fixed the bug and added the address form

--- address/formController.js
+++ address/formController.js
@@ -1,16 +1,18 @@
 angular.module('app')
     .controller('formController',function($state) {
-      state.go('addressDetails')
+
       var vm = this;
       vm.master = {};
 
       vm.update = function(user) {
-        vm.master = angular.copy(user);
+
+        vm.master = angular.copy(user); //no paglu :D
+        console.log(vm, user);
       };
 
       vm.reset = function() {
         vm.user = angular.copy(vm.master);
       };
 
-      vm.reset();
+      vm.reset(); //what is view model any variable name
     });
