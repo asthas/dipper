b60ff6a7fdcfd90d95bee3d4f7436f4819ee7c34
Astha Sharma <a@astha.me>
2016-01-15 20:10:51 +0530

    Added functionality to increment and decrement buttons

--- address/formController.js
+++ address/formController.js
@@ -2,7 +2,16 @@ angular.module('app')
     .controller('formController',function($state) {
 
       var vm = this;
+      vm.count = 1;
       vm.addresses = [];
+
+      vm.add = function() {
+        vm.count += 1;
+      }
+
+      vm.remove = function() {
+        vm.count -= 1;
+      }
 
       vm.update = function() { 
         console.log(vm);
