eda3939fd01a9eff7903a9640daa7b475bbbf22f
Astha Sharma <a@astha.me>
2016-01-15 21:04:16 +0530

    Added functionality for adding multiple address fields

--- address/formController.js
+++ address/formController.js
@@ -4,18 +4,13 @@ angular.module('app')
       var vm = this;
 
       vm.add = function() {
-        console.log('before add', vm.range);
         vm.range.push(vm.range.length);
-        console.log('after add', vm.range);
       }
 
       vm.remove = function() {
         if (vm.range.length == 1) {
-          console.log('not doing anything', vm.range);
         } else {  
-          console.log('before remove', vm.range);
           vm.range.splice(-1, 1);
-          console.log('after remove', vm.range);
         }
       }
 
